a1acb965ce98075d9d69965a81d8059afa31a7fc
RanoKur <79922708+RanoKur@users.noreply.github.com>
2021-10-29 11:05:04 +0600

    Add files via upload

---
+++ Lab2 C#/Lab2.2/Lab2.2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with line", "Line Title");
+            // the first parameter specifies the dialog box text, the second parameter specifies the title text
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a string and different buttons", "Title string", MessageBoxButtons.OKCancel);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.AbortRetryIgnore);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.OK);
+            // MessageBoxButtons determines which buttons will be located on the dialog box
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dialog box with a line and different buttons", "Title bar", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            MessageBox.Show("Dialog box with a line and different buttons", "Title line", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+            // MessageBoxIcon defines the dialog box icon, the data is in the Enum enumeration type
+        }
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Save data", "Attention!", MessageBoxButtons.YesNoCancel);
+            if (result == DialogResult.OK)
+                MessageBox.Show("Data saved");
+            if (result == DialogResult.No)
+                MessageBox.Show("Data not saved");
+            if (result == DialogResult.Cancel)
+                MessageBox.Show("Saving data canceled by the user");
+        }
+
+
+    }
+}
